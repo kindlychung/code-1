80455d59360ebf6c04bb66f94b5af76be04e97ce
keenz <kindlychung@gmail.com>
2020-03-07 17:51:32 +0100

    fix a bug where multiple words in macro are not allowed

--- ch3/ch3-anystring-macro.rs
+++ ch3/ch3-anystring-macro.rs
@@ -1,10 +1,20 @@
 macro_rules! string {
-    ($x:expr) => ( // <1>
-        String::from(stringify!($x)); // <2>
-    )
+    ( $($x:expr) * ) => {
+        {
+            let mut s = String::new();
+            $(
+                let s0 = stringify!($x);
+                println!("Found: {}", &s0);
+                s.push_str(s0); // <2>
+                s.push(' ');
+            )*
+            s.pop();
+            s
+        }
+    };
 }
 
 fn main() {
-    let s = string!(hello there);
+    let s = string!(hello there how are you?);
     println!("{}", s);
 }
