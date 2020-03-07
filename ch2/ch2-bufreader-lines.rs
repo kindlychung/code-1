2626ba46356431dff73ce5394998a41b8b52ce3c
keenz <kindlychung@gmail.com>
2020-03-07 14:50:11 +0100

    ignore case of readme.md

--- ch2/ch2-bufreader-lines.rs
+++ ch2/ch2-bufreader-lines.rs
@@ -3,11 +3,15 @@ use std::io::BufReader;
 use std::io::prelude::*;
 
 fn main() {
-  let f = File::open("readme.md").unwrap();
-  let reader = BufReader::new(f);
+    let filename = "README.md";
+    let filename_lower = filename.to_lowercase();
+    let f = File::open(filename).or(
+        File::open(filename_lower)
+    ).expect(&format!("Failed to open file: {}", filename));
+    let reader = BufReader::new(f);
 
-  for line_ in reader.lines() { // <1>
-    let line = line_.unwrap(); // <2>
-    println!("{} ({} bytes long)", line, line.len());
-  }
+    for line_ in reader.lines() { // <1>
+        let line = line_.unwrap(); // <2>
+        println!("{} ({} bytes long)", line, line.len());
+    }
 }
