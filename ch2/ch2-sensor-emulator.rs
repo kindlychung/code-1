733c0bcd71ad787319c2d42da819254afb282f75
keenz <kindlychung@gmail.com>
2020-03-07 16:11:49 +0100

    format the numbers

--- ch2/ch2-sensor-emulator.rs
+++ ch2/ch2-sensor-emulator.rs
@@ -14,23 +14,23 @@ fn main() {
 }
 
 fn pause(step: usize) {
-  let delay_ms = if step % 4 == 0 {
-      500
-  } else if step % 7 == 0 {
-      1000
-  } else {
-      3
-  };
+    let delay_ms = if step % 4 == 0 {
+        500
+    } else if step % 7 == 0 {
+        1000
+    } else {
+        3
+    };
 
-  let delay = time::Duration::from_millis(delay_ms);
-  thread::sleep(delay);
+    let delay = time::Duration::from_millis(delay_ms);
+    thread::sleep(delay);
 }
 
 fn take_reading(x: f64) -> f64 {
-  // sin(x) + sin(π / sin(πx))
-  x.sin()   + (PI / (x*PI).sin()).sin()
+    // sin(x) + sin(π / sin(πx))
+    x.sin()   + (PI / (x*PI).sin()).sin()
 }
 
 fn print_reading(reading: f64) {
-  println!("{}", reading);
+    println!("{:+.4}", reading);
 }
